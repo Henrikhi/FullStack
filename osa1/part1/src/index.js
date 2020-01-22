85eb070744d50050dddc7ed30f7f935763aa3d83
Henrik <henrik.hirvonen77@gmail.com>
2020-01-22 18:10:01 +0200

    1.10 tehty

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,10 +1,134 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+        </div>
+        )
+    }
+
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+const App = (props) => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        setLeft(left + 1)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        setRight(right + 1)
+    }
+
+
+    return (
+        <div>
+            <div>
+                {left}
+                <Button onClick={handleLeftClick} text='left' />
+                <Button onClick={handleRightClick} text='right' />
+                {right}
+                <History allClicks={allClicks} />
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
+
+
+
+
+
+/*
+const Display = (props) => {
+    return (
+        <div>{props.counter}</div>
+    )
+}
+
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick}>
+            {props.text}
+        </button>
+    )
+}
+
+
+const App = (props) => {
+    const [counter, setCounter] = useState(0)
+
+    const increaseByOne = () => setCounter(counter + 1)
+    const decreaseByOne = () => setCounter(counter - 1)
+    const setToZero = () => setCounter(0)
+
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button
+                handleClick={increaseByOne}
+                text='plus'
+            />
+            <Button
+                handleClick={setToZero}
+                text='zero'
+            />
+            <Button
+                handleClick={decreaseByOne}
+                text='minus'
+            />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
+*/
+
+
+
+/*
+const Hello = ({ name, age }) => {
+
+    const bornYear = () => new Date().getFullYear() - age
+
+
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old
+            </p>
+            <p>
+                So you were probably born {bornYear()}
+            </p>
         </div>
     )
 }
@@ -12,13 +136,15 @@ const Hello = (props) => {
 const App = () => {
     const nimi = 'Pekka'
     const ika = 10
+
     return (
         <div>
             <h1>Greetings</h1>
             <Hello name="Maya" age={26 + 10} />
-            <Hello name = {nimi} age={ika} />
+            <Hello name={nimi} age={ika} />
         </div>
     )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+*/
---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Statistics = (props) => {
+
+    const good = props.good
+    const neutral = props.neutral
+    const bad = props.bad
+    const all = good + neutral + bad
+    const avg = (good - bad) / all
+    const pos = (100 * good) / all
+
+
+
+
+
+    if (good === 0 && neutral === 0 && bad === 0) {
+
+        return (
+            <div>
+                < h2 > statistics</h2 >
+
+                <p>
+                    No feedback given
+                </p>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            < h2 > statistics</h2 >
+
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='all' value={all} />
+            <StatisticLine text='average' value={avg} />
+            <StatisticLine text='positive' value={pos} />
+
+        </div>
+    )
+}
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+
+const StatisticLine = ({ text, value }) => {
+    return (<p>
+        {text} {value}
+    </p>)
+}
+
+
+
+const App = () => {
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <div>
+
+                <h2>Give feedback</h2>
+
+                <Button onClick={handleGoodClick} text='good' />
+                <Button onClick={handleNeutralClick} text='neutral' />
+                <Button onClick={handleBadClick} text='bad' />
+
+                <Statistics good={good} neutral={neutral} bad={bad} />
+
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root')
+)
